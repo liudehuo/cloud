3c72b7cbe77b7361265c4665ce55e96effdc1a6b
liudehuo <732085701@qq.com>
2022-10-19 10:47:48 +0800

    初始化远程仓库

---
+++ cloud-provider-payment8001/src/main/java/com/ldh/spring/cloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.ldh.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 支付模块主启动类
+ * @author liudehuo
+ * @since 1.0
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
