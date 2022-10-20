6ce073930ad1261d16c6f2fe6ba3d7b803752d1e
liudehuo <732085701@qq.com>
2022-10-20 10:02:03 +0800

    服务提供者8001入驻Eureka

--- cloud-eureka-server7001/src/main/java/com/ldh/spring/cloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/ldh/spring/cloud/EurekaMain7001.java
@@ -2,7 +2,7 @@ package com.ldh.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 支付模块主启动类
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @since 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
