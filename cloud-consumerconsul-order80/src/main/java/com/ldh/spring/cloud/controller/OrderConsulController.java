72155115344eb66981d114be49cf6fbe936c3be5
liudehuo <732085701@qq.com>
2022-10-21 15:10:36 +0800

    将服务注册到consul中

---
+++ cloud-consumerconsul-order80/src/main/java/com/ldh/spring/cloud/controller/OrderConsulController.java
@@ -0,0 +1,25 @@
+package com.ldh.spring.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class OrderConsulController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/consul")
+    public String paymentInfo() {
+        return restTemplate.getForObject(INVOKE_URL + "/payment/consul",String.class);
+        // return "spring cloud with consul:" + INVOKE_URL +"\t" + UUID.randomUUID();
+    }
+}
