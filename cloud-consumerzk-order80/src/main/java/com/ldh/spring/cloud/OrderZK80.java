42f64855ec54047487a98b317579f5fe09a6ad71
liudehuo <732085701@qq.com>
2022-10-21 11:05:15 +0800

    将payment与consumer注册进zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/ldh/spring/cloud/OrderZK80.java
@@ -0,0 +1,11 @@
+package com.ldh.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderZK80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZK80.class,args);
+    }
+}
