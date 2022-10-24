c5fadae907eb35e74eb5fac1bf17ef668f89bec5
liudehuo <732085701@qq.com>
2022-10-24 15:02:54 +0800

    Ribbon的负载均衡替换

--- cloud-consumer-order80/src/main/java/com/ldh/spring/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/ldh/spring/cloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.ldh.spring.cloud;
 
+import com.ldh.spring.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * 消费模块主启动类
@@ -11,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
---
+++ cloud-consumer-order80/src/main/java/com/ldh/spring/myrule/MySelfRule.java
@@ -0,0 +1,16 @@
+package com.ldh.spring.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule() {
+        // 负载均衡机制
+        return new RandomRule();
+    }
+}
